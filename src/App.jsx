19e0d25cdc42e48ebb98b629d0497b840833cd36
Sherief Elnahas <sheriefelnahas5@gmail.com>
2022-09-06 17:08:29 +0200

    card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import './App.css';
 
-import Info from './Info';
-import Section from './Section';
-import Footer from './Footer';
+import Card from './Card';
 
 function App() {
   return (
     <div className="app">
-      <Info />
-      <Section title="About" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum sit blanditiis ducimus labore sunt placeat veniam iusto tempore ex dolorem."/>
-      <Section title="Interests" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum sit blanditiis ducimus labore sunt placeat veniam iusto tempore ex dolorem."/>
-      <Footer />
+    <Card />
     </div>
   )
 }
---
+++ src/Card.jsx
@@ -0,0 +1,16 @@
+import Info from './Info';
+import Section from './Section';
+import Footer from './Footer';
+
+function Card() {
+    return (
+      <div className="app">
+        <Info />
+        <Section title="About" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum sit blanditiis ducimus labore sunt placeat veniam iusto tempore ex dolorem."/>
+        <Section title="Interests" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum sit blanditiis ducimus labore sunt placeat veniam iusto tempore ex dolorem."/>
+        <Footer />
+      </div>
+    )
+}
+
+export default Card;
